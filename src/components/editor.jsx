1d53716668bec8e6c41d6dbdb556b733babd20be
Baldvin Mar Smarason <bsmarason@zendesk.com>
2016-04-08 18:44:14 -0700

    Style changes

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,23 +1,35 @@
 // Dependencies
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Editor, EditorState} from 'draft-js'
 
 // Module definition
 class RichEditor extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     // Store state
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {editorState: EditorState.createEmpty()}
 
     // Event handlers
-    this.onChange = (editorState) => this.setState({editorState});
+    this.onChange = (editorState) => this.setState({editorState})
   }
 
   render() {
-    const {editorState} = this.state;
-    return <Editor editorState={editorState} onChange={this.onChange} />;
+
+    var containerStyle = {
+      height: 200,
+      width: 400,
+      background: 'red',
+      overflow: 'auto'
+    };
+
+    const { editorState } = this.state
+    return (
+      <div className="editor">
+        <Editor editorState={editorState} onChange={this.onChange} placeholder="something" />
+      </div>
+    )
   }
 }
 
