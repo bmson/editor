4fade7c89ae548ed8007c0c5a9f42d4a5b92d3c8
Baldvin Mar Smarason <bsmarason@zendesk.com>
2016-04-18 15:40:15 -0700

    Rewrite compiler

--- src/components/compiler.jsx
+++ src/components/compiler.jsx
@@ -24,23 +24,26 @@ const addInline = (block, dictionary = {}, i = 0) => {
   const text = block.text.charAt(i)
 
   //
-  const open = offsets.map((offset) => {
+  const open = offsets.map(offset => {
 
     if(i === offset.start && dictionary[offset.style])
-      return dictionary[offset.style].inner[0]
+      return { type:0, style: offset.style }
+      //dictionary[offset.style].inner[0]
 
   })
 
   //
-  const close = offsets.map((offset) => {
+  const close = offsets.map(offset => {
 
     if(i+1 === offset.end && dictionary[offset.style])
-      return dictionary[offset.style].inner[1]
+      return { type:1, style: offset.style }
+      // dictionary[offset.style].inner[1]
 
   })
 
   //
-  return [].concat(open, text, close).join('')
+  return { open, text, close }
+  //return [].concat(open, text, close).join('')
 
 }
 
@@ -85,11 +88,41 @@ export default (state, dictionary = {}) => {
     const next = blocks[i + 1] || {}
 
     //
-    const fn = (value, i) => addInline(block, dictionary, i)
-    const text = [...block.text].map(fn).join('')
+    //const fn = (value, i) => {addInline(block, dictionary, i)
+    //const text = [...block.text].map(fn).join('')
 
     //
-    return addBlock(text, dictionary, {
+    let list = []
+    const callback = (value, i) => {
+      const r = addInline(block, dictionary, i)
+      if (r.open.length) list.push(r.open)
+      list.push(r.text)
+      if (r.close.length) list.push(r.close)
+    }
+
+    const text = [...block.text].forEach(callback)
+
+    list = [].concat.apply([], list)
+    list = list.filter(Boolean)
+
+    let merge = ['']
+    let step = 0;
+
+    list.forEach(function(l, i) {
+      if (l instanceof Object) {
+        merge.push(l)
+        merge.push('')
+        step = step + 2
+      } else {
+        merge[step] += l
+      }
+    })
+
+    console.log(merge)
+
+    //
+    //return addBlock(text, dictionary, {
+    return addBlock(block.text, dictionary, {
       current:  block,
       previous: blocks[i - 1] || {},
       next:     blocks[i + 1] || {},
