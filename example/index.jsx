d3dae44e96d592acc0fea22857e0407531353d55
Baldvin Mar Smarason <bmson@bmson.com>
2016-04-29 07:01:14 -0700

    Pass state from editor

--- example/index.jsx
+++ example/index.jsx
@@ -1,39 +1,35 @@
 // Global dependencies
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Container, Preview } from 'editor'
+import { Editor, Toolbar, Preview } from 'editor'
 
 // Local dependencies
-import HotContent from './components/hotContent.jsx'
+import Definition from './components/definition.jsx'
 
 // Module definition
 export default class App extends React.Component {
 
   //
   state = {
-    value: null
+    editor: Editor.createEmpty(),
+    result: null
   }
 
   //
-  update = () => {
-
-    //
-    const { editor } = this.refs
-
-    //
-    this.setState({
-      value: editor.compile()
-    })
-
+  update = (data) => {
+    const result = data.compile()
+    this.setState({ result })
   }
 
   render = () =>
     <div>
-      <Container ref = 'editor'  onChange = { this.update }>
-        <HotContent className = 'codeBlock' tooltip = 'Hot Content' />
-      </Container>
+      <Editor state = {this.state.editor}  onChange = {this.update} />
 
-      <Preview value = { this.state.value } />
+      <Toolbar state = {this.state.editor}>
+        <Definition className = 'codeBlock' tooltip = 'Hot Content' />
+      </Toolbar>
+
+      <Preview value = { this.state.result } />
     </div>
 
 }
