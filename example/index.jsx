14b81d058f3adc8ad3445d3187e9bfe26694f568
Baldvin Mar Smarason <bmson@bmson.com>
2016-04-29 07:17:13 -0700

    Add onChange to toolbar

--- example/index.jsx
+++ example/index.jsx
@@ -16,16 +16,14 @@ export default class App extends React.Component {
   }
 
   //
-  update = (data) => {
-    const result = data.compile()
-    this.setState({ result })
-  }
+  update = (editor) => this.setState({ result: editor.compile() })
+  format = (editor) => this.setState({ editor })
 
   render = () =>
     <div>
-      <Editor state = {this.state.editor}  onChange = {this.update} />
+      <Editor state = {this.state.editor} placeholder = 'Comment...' onChange = {this.update} />
 
-      <Toolbar state = {this.state.editor}>
+      <Toolbar state = {this.state.editor} onChange = {this.format}>
         <Definition className = 'codeBlock' tooltip = 'Hot Content' />
       </Toolbar>
 
