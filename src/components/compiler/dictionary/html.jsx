049aeb3e6505321a2a4c6d446d80036587802802
Baldvin Mar Smarason <bsmarason@zendesk.com>
2016-05-03 16:30:00 -0700

    Cleanup inline functions

--- src/components/compiler/dictionary/html.jsx
+++ src/components/compiler/dictionary/html.jsx
@@ -2,27 +2,27 @@ export default {
 
   // Block
   "unstyled": e =>
-    (e.firstChild ? '<p>' : ' ')
+    (e.firstChild ? '<p>' : '')
   + (e.nodeValue)
   + (e.lastChild ? '</p>' : '<br/>'),
 
   "blockquote": e =>
-    (e.firstChild ? '<blockquote>' : ' ')
+    (e.firstChild ? '<blockquote>' : '')
   + (e.nodeValue)
   + (e.lastChild ? '</blockquote>' : '<br/>'),
 
   "code-block": e =>
-    (e.firstChild ? '<pre>' : ' ')
+    (e.firstChild ? '<pre>' : '')
   + (e.nodeValue)
   + (e.lastChild ? '</pre>' : '<br/>'),
 
   "ordered-list-item": e =>
-    (e.firstChild ? '<ol><li> ' : ' <li> ')
+    (e.firstChild ? '<ol><li>' : ' <li>')
   + (e.nodeValue)
   + (e.lastChild ? '</li></ol>' : '</li>'),
 
   "unordered-list-item": e =>
-    (e.firstChild ? '<ul><li> ' : ' <li> ')
+    (e.firstChild ? '<ul><li>' : ' <li>')
   + (e.nodeValue)
   + (e.lastChild ? '</li></ul>' : '</li>'),
 
--- src/components/compiler/type/inline.jsx
+++ src/components/compiler/type/inline.jsx
@@ -1,29 +1,34 @@
-//
-const toggle = (array = [], value = '') => {
-  var index = array.indexOf(value);
+// Toggle value in array
+const toggleArray = (array = [], value = '') => {
 
-  var copy = [].concat(array);
+  // Get index of current value and clone array
+  const index = array.indexOf(value);
+  const copy  = [...array];
 
-  if (index === -1) {
-    copy.push(value);
-  } else {
-    copy.splice(index, 1);
-  }
+  // Add value if it isnot found in array, otherwise remove it
+  (index < 0) ? copy.splice(0, 0, value) : copy.splice(index, 1)
 
-  return copy;
+  // Return transformed array
+  return copy
+
 }
 
 //
 const inject = (input, child, index) => {
 
-  //
+  // Collect child attributes and clone array
   const { previous, current, next } = child
+  const copy = [...input]
 
-  //
+  // Filter out values that don't have text attribute
+  // so we're not inserting empty style tags
   const array = [previous, current, next].filter(n => n.text)
 
-  //
+  // TODO: Remove input from here
   input.splice(index, 1, ...array)
+
+  //
+  return { child, copy }
 
 }
 
@@ -45,24 +50,20 @@ const splitter = (input, position, iterator) => {
   //
   const child = {
     previous:  { text: value.slice(0, from), type: curType, style:curStyle }, // remove position from curStyle ????
-    current:   { text: value.slice(from, to), type: toggle(curType, position.style), style: position.style },
+    current:   { text: value.slice(from, to), type: toggleArray(curType, position.style), style: position.style },
     next:      { text: value.slice(to, value.length), type: curType, style:curStyle }, // remove position from curStyle
   }
 
   //
-  inject(input, child, index)
-
-  //
-  return child
+  return inject(input, child, index)
 
 }
 
 //
-export const walker = (input, position, iterator) => {
+const walker = (input, position, { index, adjust } = { index: 0, adjust: 0 }) => {
 
   //
   const { from, to } = position
-  const { index, adjust } = iterator
 
   //
   const length = input[index] && input[index].text.length
@@ -70,7 +71,7 @@ export const walker = (input, position, iterator) => {
   //
   if (from - adjust >= length) {
 
-    walker(input, position, {
+    return walker(input, position, {
       index: index + 1,
       adjust: adjust + length
     })
@@ -78,19 +79,23 @@ export const walker = (input, position, iterator) => {
   } else {
 
     // Matched
-    const child = splitter(input, position, iterator)
+    const { child, copy } = splitter(input, position, { index, adjust })
 
     //
     if (child.previous.text && child.current.text && !child.next.text) {
 
-      walker(input, {
-        from: from + (child.current).text.length,
-        to: to,
-        style: (child.current).style
+      return walker(input, {
+        from:  from + (child.current).text.length,
+        to:    to,
+        style: child.current.style
       }, {
-        index: index + 1,
+        index:  index + 1,
         adjust: adjust + length - (child.current).text.length
       })
+
+    } else {
+
+      return input
 
     }
 
@@ -98,46 +103,51 @@ export const walker = (input, position, iterator) => {
 
 }
 
+const setDictionary = ({ text, type }, dictionary) => {
+
+  //
+  let val = text
+
+  //
+  type.forEach(o => {
+    val = dictionary[o] && dictionary[o].call(this, { nodeValue: val }) || val
+  })
+
+  return val
+
+}
+
 //
 export default (block, dictionary = {}) => {
 
-  const text = [{ text: block.text, type:[''] }]
+  //
+  const text = [{
+    text: block.text,
+    type: ['']
+  }]
+
+  //
   const ranges = block.inlineStyleRanges
 
-  ranges.map(range => {
+  //
+  ranges.forEach(range => {
 
     //
     const position = {
-      from: range.offset,
-      to:   range.offset + range.length,
+      from:  range.offset,
+      to:    range.offset + range.length,
       style: range.style
     }
 
     //
-    const iterator = {
-      index:  0,
-      adjust: 0
-    }
-
-    //
-    walker(text, position, iterator)
+    walker(text, position)
 
   })
 
-
   //
-  return text.map(i => {
+  return text.map(value => {
 
-    //
-    let val = i.text
-
-    //
-    i.type.forEach(o => {
-      val = dictionary[o] && dictionary[o].call(this, { nodeValue: val }) || val
-    })
-
-    //
-    return val;
+    return setDictionary(value, dictionary)
 
   }).join('') || block.text
 
