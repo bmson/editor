406b4dfcd19a731f2b8502f4c696e2392c74be67
Baldvin Mar Smarason <bmson@bmson.com>
2016-05-08 08:09:02 -0700

    Style preview module

--- src/preview/index.jsx
+++ src/preview/index.jsx
@@ -7,11 +7,28 @@ import style from './stylesheet.css'
 // Module definition
 export default class extends React.Component {
 
+  //
+  state = {
+    output: ''
+  }
+
+  //
+  componentWillReceiveProps = (props) => {
+
+    //
+    const split  = props.value.split('\n')
+    const output = split.map((string, index) => <div key = { index }>{ string || String.fromCharCode(0) }</div>)
+
+    //
+    this.setState({ output })
+
+  }
+
   // Render component
   render = () =>
     <div className = { style.preview }>
       <div className = 'selection'>{ this.props.children }</div>
-      <pre className = 'output'>{ this.props.value }</pre>
+      <pre className = 'output'>{ this.state.output }</pre>
     </div>
 
 }
